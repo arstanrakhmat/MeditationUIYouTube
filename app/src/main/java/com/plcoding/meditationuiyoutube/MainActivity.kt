59375c950f8a18424510551c8c858d9e8023c4b3
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2024-01-07 11:39:16 +0300

    Add greeting and chip sections design

--- app/src/main/java/com/plcoding/meditationuiyoutube/MainActivity.kt
+++ app/src/main/java/com/plcoding/meditationuiyoutube/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import com.plcoding.meditationuiyoutube.ui.HomeScreen
 import com.plcoding.meditationuiyoutube.ui.theme.MeditationUIYouTubeTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MeditationUIYouTubeTheme {
-
+                HomeScreen()
             }
         }
     }
